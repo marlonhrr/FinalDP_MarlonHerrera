1b367bc10fe7153defbbdb9973a92880ca664404
MARLON ST. HERRERA OSORIO <marlonherrera163922@correo.itm.edu.co>
2019-11-24 18:27:42 -0500

    Finish Project Android

---
+++ TipCalculator.Android/Views/TipView.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using TipCalculator.Core.ViewModels;
+using Android.Content.PM;
+using MvvmCross.Platforms.Android.Core;
+using TipCalculator.Core;
+using TipCalculator.Android;
+
+[Activity(Label = "@string/app_name")]
+public class TipView : MvxActivity<TipViewModel>
+{
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        this.SetContentView(Resource.Layout.TipPage);
+    }
+}
