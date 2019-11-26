ccab6aff629cc3d0a79464ff37e7069b82298569
MARLON ST. HERRERA OSORIO <marlonherrera163922@correo.itm.edu.co>
2019-11-26 17:31:17 -0500

    Test 5 - Folder Service

--- QuadraticEquations.Core/Services/IQuadraticEquations.cs
+++ QuadraticEquations.Core/Services/IQuadraticEquations.cs
@@ -6,6 +6,7 @@ namespace QuadraticEquations.Core.Services
 {
     public interface IQuadraticEquations
     {
-        
+        double QuadraticEquationsX1(double number1, double number2, double number3);
+        double QuadraticEquationsX2(double number1, double number2, double number3);
     }
 }
---
+++ QuadraticEquations.Core/Services/QuadraticEquations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquations.Core.Services
+{
+    public class QuadraticEquations
+    {
+        
+    }
+}
